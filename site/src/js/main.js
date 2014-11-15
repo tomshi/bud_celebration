86a65ddb8eb929cc783fb01414e3fc4586cd37cc
Zane Yao <yaoazhen@gmail.com>
2014-11-15 13:45:10 +0800

    first animation

--- site/src/js/main.js
+++ site/src/js/main.js
@@ -1,7 +1,43 @@
 $(function () {
 
 	var $frame1 = $('#frame1');
+	var $bg_frame1 = $('.bg1', $frame1);
+	var $model1 = $('.model1', $frame1);
+	var $model2 = $('.model2', $frame1);
+
 	var $hand = $('#hand');
+
+
+	var preload = function () {
+		var imageNames = [
+			'img/frame1-bg.jpg',
+			'img/frame1-bg2.jpg',
+			'img/hand.png',
+			'img/model1.png',
+			'img/model2.png'
+		];
+
+		var imagesCount = imageNames.length;
+		var loadedImagesCount = 0;
+		var $preloader = $('.preload');
+		var images = [];
+
+		for (var i = 0; i < imagesCount; i++) {
+			images[i] = new Image();
+			images[i].src = imageNames[i];
+			images[i].onload = function () {
+
+				$preloader.find('p').text('正在加载：' + Math.ceil(100 * (++loadedImagesCount / imagesCount)) + '%');
+
+				if (loadedImagesCount >= imagesCount) {
+					setTimeout(function () {
+						$frame1.show().trigger('start');
+					}, 300);
+				}
+			};
+		}
+	};
+
 
 	var handup = function (cb) {
 		$hand.velocity({
@@ -18,43 +54,57 @@ $(function () {
 	};
 
 
-	$frame1.velocity({
-//		"translateX": "-160px",
-//		"translateZ": "20px",
-		scale: 1.2
-	}, 1500, function () {
-		handup(function () {
+	var frame1_start = function () {
+		$bg_frame1.add($model1).add($model2).velocity({
+			"translateZ": "180px"
+		}, 1500, function () {
 
-			$('.model1', $frame1).velocity({
-				"translateX": "70px",
-				"translateY": "20px",
-				"translateZ": "50px",
-				"scale": 1.8
-			}, 1500);
-			handdown();
 
-			$frame1.velocity({
-				scale: 1.8
-			}, 1600, function () {
-				handup(function () {
-					handdown();
+			handup(function () {
 
-					$('.model1', $frame1).velocity({
-						"translateX": "100px",
-						"translateY": "90px"
-					}, 1500);
+				handdown();
 
-					$('.model2', $frame1).velocity({
-						"translateX": "-80px",
+				$model1.velocity({
+					"translateZ": "800px"
+				}, 1700);
 
-						"translateZ": "50px"
-					}, 1500);
-					$frame1.velocity({
-						scale: 3.5
-					}, 1900);
+				$model2.velocity({
+					"translateZ": "390px"
+				}, 1500);
+
+				$bg_frame1.velocity({
+					"translateZ": "300px"
+				}, 1600, function () {
+
+					handup(function () {
+						handdown();
+
+						$('.model1', $frame1).velocity({
+							"translateZ": "1200px"
+						}, 500, "linear");
+
+						$('.model2', $frame1).velocity({
+							"translateZ": "1000px"
+						}, 1000, "linear");
+
+						$bg_frame1.velocity({
+							"translateZ": "499px"
+						},400, "linear", function () {
+
+							$bg_frame1.addClass('bg2').css({
+								transform: 'translateZ(0px)'
+							}).removeData().velocity({
+								"translateZ": "800px"
+							}, 700, "easeOutSine");
+						});
+					});
 				});
-			});
 
+			});
 		});
-	});
+	};
+
+
+	$frame1.on('start', frame1_start);
+	preload();
 });
