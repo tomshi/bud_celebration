1261b8a604b9445a7bd4977ce0077580d8430e02
Andrew Bao <andrewbao.it@gmail.com>
2014-11-22 16:33:56 +0800

    Added folder 'font' into GruntFile.js Used final Baidu and GA accounts

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function (grunt) {
 					{
 						expand: true,
 						cwd: '<%= config.app %>',
-						src: ['api/**/*', 'css/**/*.css', 'js/main.js', 'js/vendor.js', 'js/plugins.js', '{fonts,icons,sprites,img}/**/*', '*.html', '*.xml'],
+						src: ['api/**/*', 'css/**/*.css', 'js/main.js', 'js/vendor.js', 'js/plugins.js', '{fonts,font,icons,sprites,img}/**/*', '*.html', '*.xml'],
 						dest: '<%= config.dist %>'
 					}
 				]
