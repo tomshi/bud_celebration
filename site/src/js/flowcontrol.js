2e2c65e33b247f2732e4c8ba314e2cb0369657a3
samuel.leung <samuel.leung@163.com>
2014-11-30 20:55:22 +0800

    fix ie 8 bug

--- site/src/js/flowcontrol.js
+++ site/src/js/flowcontrol.js
@@ -102,7 +102,7 @@ function submitUserData() {
 
 
 function activeSubmitButton() {
-    if ($("#username").val() !== "" && $("#purpose").val() !== "" && $("#time-m").val() !== "" && $("#time-d").val() !== "") {
+    if ($("#username").val() !== "" && $("#purpose").val() !== "" && $("#time-m").val() !== "" && $("#time-d").val() !== "" && $("#username").val() != "为谁欢庆" && $("#purpose").val() != "庆祝什么") {
         $("#submit").addClass("active");
     } else {
         $("#submit").removeClass("active");
