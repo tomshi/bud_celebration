dc4fd68abc38c345bc5c330587d3fddf2cfdb025
Zane Yao <yaoazhen@gmail.com>
2014-12-01 02:27:06 +0800

    add console for preload

--- site/src/js/movie-preload.js
+++ site/src/js/movie-preload.js
@@ -53,6 +53,7 @@ var movieReady = function () {
 
 
 	var count = function () {
+		console.log(loadedResourceCount, resourceCount);
 		if (loadedResourceCount >= resourceCount) {
 
 			$movie_preload.addClass('ready');
