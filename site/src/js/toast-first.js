85de00d90965e1304fb4ce77944af4a043eb84e9
samuel.leung <samuel.leung@163.com>
2014-12-08 11:24:48 +0800

    add mobile animation

--- site/src/js/toast-first.js
+++ site/src/js/toast-first.js
@@ -72,40 +72,39 @@ $(function () {
 	};
 
 	var toast = function () {
-		$hand1.velocity({
-			translateX: "75%",
-			translateY: "-120%"
-		}, {
-			duration: time,
-			easing: 'easeOutQuint'
-		});
-		$hand2.velocity({
-			translateX: "97%",
-			translateY: "-90%"
-		}, {
-			duration: time + 200,
-			easing: 'easeOutQuint'
-		});
+        $hand1.velocity({
+            translateX: "93%",
+            translateY: "-124%"
+        }, {
+            duration: time,
+            easing: 'easeOutQuint'
+        });
+        $hand2.velocity({
+            translateX: "100%",
+            translateY: "-90%"
+        }, {
+            duration: time + 200,
+            easing: 'easeOutQuint'
+        });
 
-		$hand3.velocity({
-			translateX: "-79%",
-			translateY: "-96%"
-		}, {
-			duration: time + 200,
-			easing: 'easeOutQuint'
-		});
+        $hand3.velocity({
+            translateX: "-110%",
+            translateY: "-90%"
+        }, {
+            duration: time + 200,
+            easing: 'easeOutQuint'
+        });
 
-		$hand4.velocity({
-			translateX: "-12%",
-			translateY: "-112%"
-		}, {
-			duration: time + 200,
-			easing: 'easeOutQuint'
-		});
+        $hand4.velocity({
+            translateX: "-90%",
+            translateY: "-30%"
+        }, {
+            duration: time + 200,
+            easing: 'easeOutQuint'
+        });
 
         $hand5.velocity({
-            translateX: "0",
-            translateY: "-82%"
+            translateY: "-80%"
         }, {
 			duration: time,
 			easing: 'easeOutQuint',
