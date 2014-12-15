c7cf1e5ee84f16bce4b810775d69d420799381e6
samuel.leung <samuel.leung@163.com>
2014-12-15 14:03:44 +0800

    fix some mobile bug for feedback.

--- site/src/js/toast-first.js
+++ site/src/js/toast-first.js
@@ -114,6 +114,10 @@ $(function () {
                     var offset = $(this).offset();
                     var relativeX = (e.pageX - offset.left);
                     var relativeY = (e.pageY - offset.top);
+                    $(".logo").velocity({
+                        "translateX": relativeX/200,
+                        "translateY": relativeY/100
+                    },0.000000001);
                     $("#hands").velocity({
                         "translateX": -relativeX/100,
                         "translateY": -relativeY/100
